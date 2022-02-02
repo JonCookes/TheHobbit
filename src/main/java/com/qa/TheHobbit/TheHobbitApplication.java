90621d9ce24f5a5d1a6d999e43b250d085f265b5
FIRST_NAME LAST_NAME <My_NAME@example,com>
2022-02-02 20:50:12 +0000

    Initial commit

---
+++ src/main/java/com/qa/TheHobbit/TheHobbitApplication.java
@@ -0,0 +1,13 @@
+package com.qa.TheHobbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHobbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHobbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/TheHobbit/TheHobbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.TheHobbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHobbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
